48c47a0f4dbe0ed853db8ff2260b9ec192072daf
Minh Duc <minhducdang5@gmail.com>
2024-06-10 02:01:23 +0700

    mm

---
+++ README.md
@@ -0,0 +1,26 @@
+![og](https://github.com/pakastin/flanets/assets/1475902/ecdf981f-d265-4d6b-9661-be04a9a006c3)
+
+# Flanets.io
+Free and open source 2D space flight simulator. Gravity-wise should be pretty realistic (except 2D, but orbits actually are quite 2D in reality). Missing athmospheric drag and such yet though. Contributions welcome!
+
+## Play!
+https://flanets.io
+
+## Install dependencies
+`npm i`
+
+## Run development environment (auto-builds JS)
+```
+npm run dev
+```
+
+––> http://localhost:8080/
+
+## Just run
+```
+npm start
+```
+
+
+## Thanks to!
+- http://www.braeunig.us/space/orbmech.htm
---
+++ server/index.js
@@ -0,0 +1,140 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+
+JWT_SECRET  = "secret";
+const PORT =  8080;
+const USERS_FILE = path.join(__dirname, '../users.json');
+
+const app = express();
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(express.static('public'));
+app.use(cookieParser());
+
+
+app.set('view engine', 'pug');
+app.set('views', 'client');
+
+
+const readUsers = () => {
+  try {
+    const data = fs.readFileSync(USERS_FILE, 'utf8');
+    return JSON.parse(data);
+  } catch (err) {
+    console.error('Error reading users file:', err);
+    return [];
+  }
+};
+
+const writeUsers = (users) => {
+  try {
+    fs.writeFileSync(USERS_FILE, JSON.stringify(users, null, 2));
+  } catch (err) {
+    console.error('Error writing to users file:', err);
+  }
+};
+
+
+const authenticateToken = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (!token) {
+    return res.redirect('/');
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.redirect('/');
+    }
+    req.user = user;
+    next();
+  });
+};
+
+// Routes
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+app.post('/register', async (req, res) => {
+  const { username, email, password } = req.body;
+
+  console.log('Received registration data:', req.body); // Log the request body
+
+  if (!username || !email || !password) {
+    return res.status(400).json({ error: 'All fields are required' });
+  }
+
+  try {
+    const users = readUsers();
+    const existingUser = users.find(user => user.email === email);
+    if (existingUser) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    console.log('Hashing password:', password); // Log the password before hashing
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = { username, email, password: hashedPassword };
+    users.push(newUser);
+    writeUsers(users);
+
+    // Automatically log the user in after registration
+    const token = jwt.sign({ id: newUser.email }, JWT_SECRET, { expiresIn: '1h' });
+    res.cookie('jwt', token, { httpOnly: true });
+    res.redirect('/game');
+  } catch (err) {
+    console.error('Registration error:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ error: 'All fields are required' });
+  }
+
+  try {
+    const users = readUsers();
+    const user = users.find(user => user.email === email);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(403).json({ error: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign({ id: user.email }, JWT_SECRET, { expiresIn: '1h' });
+    res.cookie('jwt', token, { httpOnly: true });
+    res.redirect('/game');
+  } catch (err) {
+    console.error('Login error:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/game', authenticateToken, (req, res) => {
+  res.render('game');
+});
+
+// Start server
+app.listen(PORT, (err) => {
+  if (err) {
+    throw new Error(err);
+  }
+  console.log(`Listening to port ${PORT}`);
+});
