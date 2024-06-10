d03a9e63db065851ca993925c815bf236c73a0b0
Đặng Minh Đức <114754286+p1l4ss@users.noreply.github.com>
2024-06-10 18:24:19 +0700

    Update game.js

--- client/game.js
+++ client/game.js
@@ -366,8 +366,6 @@ function updateShip (deltaTime, once) {
           $intro.textContent = 'Fly around in your space ship with realistic gravity';
         } else if (diff < 8000) {
           $intro.textContent = 'and planets where they actually are right now in our galaxy';
-        } else if (diff < 11000) {
-          $intro.textContent = 'Source open at https://github.com/p1l4ss/space-ship';
         } else {
           $intro.textContent = 'Have fun!';
         }
